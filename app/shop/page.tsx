973f8ec70559794cb9387284422586ad75d2fbbe
JeroenvElderen <jeroen.vanelderen@hotmail.com>
2026-04-23 18:41:39 +0100

    update

--- app/shop/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ShopPage() {
-  return (
-    <main className="mx-auto max-w-5xl px-6 py-20">
-      <h1 className="text-5xl font-black text-[#fff2ff]">PetPals Shop</h1>
-      <p className="mt-6 text-xl text-[#e3cbff]">Coming soon.</p>
-    </main>
-  );
-}
